c20b7789e8641fcfe3dc84e650b825509e4a4fed
BENJAMIN TAIWO <taiwoabegunde@live.com>
2016-02-16 13:38:40 +0100

    implent a function to print even numbers

--- Exercises/even_numbers.py
+++ Exercises/even_numbers.py
@@ -1,7 +1,7 @@
 def even_numbers(lower, higher):
 	if lower > higher:
-		print "error: higher must be greater than lower" 
+		print ("error: higher must be greater than lower") 
 	else:
-		for i in range(lower, higher):
-			if i%2==2:
-				print i
+		for i in range(lower, higher+1):
+			if i%2==0:
+				print (i)
